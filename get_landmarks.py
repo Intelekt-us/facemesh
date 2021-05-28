9973181511df8a79e54b990546e2238546e8088b
Skelu21 <bartek-leks@wp.pl>
2021-05-28 21:25:01 +0200

    added boundary error handling

--- get_landmarks.py
+++ get_landmarks.py
@@ -6,35 +6,42 @@ from math import atan2, acos, pi
 mp_face_mesh = mp.solutions.face_mesh
 
 vc = cv2.VideoCapture(0)
+theta_lr = 0
+theta_lu = 0
+theta_du = 0
+
 while True:
     with mp_face_mesh.FaceMesh(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as face_mesh:
 
-
         success, image = vc.read()
         if not success:
             break
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         results = face_mesh.process(image)
-        left = results.multi_face_landmarks[0].landmark[234]
-        right = results.multi_face_landmarks[0].landmark[454]
-        down = results.multi_face_landmarks[0].landmark[151]
-        up = results.multi_face_landmarks[0].landmark[9]
-        lr_vec = np.array([right.x - left.x, right.z - left.z])
-        lu_vec = np.array([right.x - left.x, right.y - left.y])
-        du_vec = np.array([up.y - down.y, up.z - down.z])
+        try:
+            landmarks = results.multi_face_landmarks[0].landmark
+            left = landmarks[234]
+            right = landmarks[454]
+            down = landmarks[151]
+            up = landmarks[9]
+            lr_vec = np.array([right.x - left.x, right.z - left.z])
+            lu_vec = np.array([right.x - left.x, right.y - left.y])
+            du_vec = np.array([up.y - down.y, up.z - down.z])
 
-        theta_lr = atan2(lr_vec[1], lr_vec[0]) * 180 / pi
-        theta_lu = atan2(lu_vec[1], lu_vec[0]) * 180 / pi
-        theta_du = atan2(du_vec[1], du_vec[0]) * 180 / pi
-        cv2.putText(image,f"YAW: {theta_lr:.0f}", (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,0,0), 2)
-        cv2.putText(image,f"ROLL: {theta_lu:.0f}", (20,80), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
-        cv2.putText(image,f"PITCH: {theta_du:.0f}", (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,255), 2)
+            theta_lr = atan2(lr_vec[1], lr_vec[0]) * 180 / pi
+            theta_lu = atan2(lu_vec[1], lu_vec[0]) * 180 / pi
+            theta_du = atan2(du_vec[1], du_vec[0]) * 180 / pi * -1
+        except:
+            cv2.putText(image, 'LOST AT: ', (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,0,0), 2)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.putText(image,f"YAW: {theta_lr:.0f}", (20,80), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,0,0), 2)
+        cv2.putText(image,f"ROLL: {theta_lu:.0f}", (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
+        cv2.putText(image,f"PITCH: {theta_du:.0f}", (20,160), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,255), 2)
         cv2.imshow('vid',image)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            break   
-
-
-# create 
-
+            break  
